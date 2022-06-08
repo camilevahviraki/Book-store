afb293b0aee736c209a3d5c49751dcf60a6dc9ce
camilevahviraki <camilevahviraki@gmail.com>
2022-06-08 17:02:21 +0200

    Finish functionnalities

--- src/modules/Books/Books.js
+++ src/modules/Books/Books.js
@@ -1,44 +1,25 @@
-import React, { Component } from 'react';
+import { useSelector } from 'react-redux';
+import React, { Component, useState } from 'react';
 import Book from './Book';
 import Form from '../form/Form';
 import Header from '../Header';
 
-export default class Books extends Component {
-  state = {
-    books: [
-      {
-        id: 1, title: 'Hunger games', currentChap: 'Chap 1', Author: 'Camilux', Completed: '15', type: 'action',
-      },
-      {
-        id: 2, title: 'Transporters', currentChap: 'Chap 3', Author: 'A7', Completed: '05', type: 'adventure',
-      },
-    ],
-  }
-
-  links = [
+export default function Books() {
+  const Bookstore = useSelector((state) => state.booksReducer);
+  const [links] = useState([
     { status: false, link: '../categories' },
     { status: true, link: '../' },
-  ]
+  ]);
 
-  addBook = (book) => {
-    const newTodo = {
-      id: this.state.books.length + 1, title: book.book, currentChap: 'Chap 1', Author: book.author, Completed: '0', type: 'action',
-    };
-    this.setState({
-      books: [...this.state.books, newTodo],
-    });
-    console.log('new', this.state.books);
-  }
-
-  render() {
-    return (
-      <div>
-        <Header obj={this.links} />
-        {this.state.books.map((book) => (
+  return (
+    <div>
+      <Header obj={links} />
+      {
+        Bookstore.map((book) => (
           <Book key={book.id} book={book} />
-        ))}
-        <Form addBook={this.addBook} />
-      </div>
-    );
-  }
+        ))
+}
+      <Form />
+    </div>
+  );
 }
--- src/modules/Categories.js
+++ src/modules/Categories.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from './Header';
+import { check } from '../redux/categories/categories';
 
-export default class Categories extends Component {
-  links = [
+export default function Categories() {
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.categories);
+  const [links] = useState([
     { status: true, link: '../categories' },
     { status: false, link: '../' },
-  ]
+  ]);
 
-  render() {
-    return (
-      <div>
-        <Header obj={this.links} />
-        <h2> Categories</h2>
-        <button type="button">check status</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header obj={links} />
+      <h2> Categories</h2>
+      <button type="button" onClick={() => dispatch(check())}>
+        check status
+      </button>
+      <h4>{categories}</h4>
+    </div>
+  );
 }
