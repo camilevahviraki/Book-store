f92166c4e362352af32653acff452bcfe502473c
camilevahviraki <camilevahviraki@gmail.com>
2022-06-08 16:58:16 +0200

    Add provider

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Books from './modules/Books/Books';
 import Categories from './modules/Categories';
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="categories" element={<Categories />} />
-    </Routes>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="categories" element={<Categories />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>,
 );
